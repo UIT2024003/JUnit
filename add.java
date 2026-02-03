b984145def3a69b88514b0d04b618202041edbe6
UIT2024003 <shravani.vetal@cumminscollege.in>
2026-02-03 14:58:37 +0530

    Successful test for Addition

---
+++ add.java
@@ -0,0 +1,9 @@
+package JUnit;
+class add{
+    public int addnumbers(int x, int y){
+        int sum=0;
+        sum=x+y;
+        System.out.println("Sum is: "+sum);
+        return sum;
+    }
+}
---
+++ addTest.java
@@ -0,0 +1,11 @@
+package JUnit;
+import org.junit.*;
+public class addTest{
+    @Test 
+    public void test1forAdd(){
+        add add_var= new add();
+        Assert.assertEquals(7, add_var.addnumbers(4, 3));
+        Assert.assertEquals(-7, add_var.addnumbers(-4, -3));
+        Assert.assertEquals(201, add_var.addnumbers(201, 0));
+    }
+}
